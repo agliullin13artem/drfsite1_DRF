b929eb567492dfaca209f91ab283236615fd134e
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-03-16 21:49:19 +0500

    authentication token , permission

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -15,12 +15,43 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 
-from women.views import WomenAPIView
+from women.views import *
+from rest_framework import routers
+
+
+# для создания собственного роутеров
+# class MyCustomRouter(routers.SimpleRouter):
+#     router = [
+#         routers.Route(
+#             url=r'^{prefix}$',
+#             mapping={'get': 'list'},
+#             name='{basename}-list',
+#             detail=False,
+#             initkwargs={'suffix': 'List'}
+#         ),
+#         routers.Route(
+#             url=r'^{prefix}/{lookup}$',
+#             mapping={'get': 'retrieve'},
+#             name='{basename}-detail',
+#             detail=True,
+#             initkwargs={'suffix': 'Detail'}
+#         ),
+#     ]
+
+# router = MyCustomRouter()
+# router.register(r'women',WomenViewSet, basename='women')
+# print(router.urls)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist', WomenAPIView.as_view(), name='')
-
-]
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
+    path('api/v1/women/', WomenAPIList.as_view()),
+    path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
+    path('api/v1/auth/', include('djoser.urls')), # пакет djoser
+    re_path(r'^auth/', include('djoser.urls.authtoken')), #пакет djoser
+    
+    ]
--- women/views.py
+++ women/views.py
@@ -1,13 +1,138 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from women.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
-from women.serializers import WomenSerializer
 
-from .models import Women
+
+from .models import Category, Women
+from .serializers import WomenSerializer
+
+
+# чтение  и добавление c ограничением либо авторизованный или только для чтения
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+# изменение 
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticated, )
+    # authentication_classes = (TokenAuthentication, )
+
+
+# удаление чтение админ и обынчый
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+
+
+
+
+
+
+
+
+# # чиение редлактиирование добаление и тд
+# class WomenViewSet(viewsets.ModelViewSet):
+#     # queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+#     def get_queryset(self):
+#         pk = self.kwargs.get('pk')
+
+#         if not pk:
+#             return Women.objects.all()[:3]
+        
+#         return Women.objects.filter(pk=pk)
+
+
+    # новый маршрут
+    # @action(method=['get'], detail=True)
+    # def category(self, request, pk=None):
+    #     cats = Category.objects.get()
+    #     return Response({'cats':[cats.name]})
+       
+    
+
+
+
+# # для чтения по GET и добавление по POST запросам все в одном
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+
+# # для измениния данных 
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+# # для удаления и чтения данных измениния многов одном
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+
+# class WomenAPIView(APIView):
+
+#     def get(self, request):
+#         w = Women.objects.all()
+#         return Response({'posts': WomenSerializer(w, many=True).data})
+
+
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response({'post': serializer.data})
+
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk', None)
+#         if not pk:
+#             return Response({'error: Метод пут не алоовер'})
+        
+#         try:
+#             instance = Women.objects.get(pk=pk)
+        
+#         except:
+#             return Response({'error: Обьект не найден'})
+
+
+#         serializer = WomenSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
+    
+
+
+#     def delete(self, request, *args, **kwargs):
+#             pk = kwargs.get('pk', None)
+#             if not pk:
+#                 return Response({'error: Метод delete не алоовер'})
+#             serializer = Women.objects.get(pk=pk).delete
+
+#             return Response({'post':'delete post' + str(pk)})
+
+#         # post_new = Women.objects.create(
+#         #     title=request.data['title'],
+#         #     content=request.data['content'],
+#         #     cat_id=request.data['cat_id'],
+#         # )
 
 
 # создаем первое api
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
